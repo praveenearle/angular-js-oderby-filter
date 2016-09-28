51119a2b0f5372d15d7a8a94133abbde47e8833a
praveenearle <praveen.earle@gmail.com>
2016-09-28 13:40:53 +0530

    angular js orderby filter
    angular js orderby filter

---
+++ angular js orderby/angular js s1a2/scripts/script.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+
+    var employees = [
+        {name: "ajay" , DOB:  ("january 24,1992"), gender : "male"},
+    {name: "rajesh" , DOB:  ("april 01,1991"), gender : "male"},
+        {name: "kajal" , DOB:  ("may 11,1993"), gender : "female"},
+];
+
+$scope.employees = employees;
+$scope.sortcolumn = "name";
+    
+
+         
+    }); 
+
